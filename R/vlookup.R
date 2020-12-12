6cf3be10fc4beef32abfc6e7f9c88a6862d2ef2d
njbultman74 <njbultman74@gmail.com>
2020-12-12 09:08:55 -0600

    Adding documentation.

--- R/vlookup.R
+++ R/vlookup.R
@@ -1,29 +1,27 @@
 #' Find/Return Values From Matching Rows in Data Frame.
-#' 
+#'
 #' Finds matching rows in specific column of data frame that matches the values specified.
-#' 
+#'
 #' @return Dataframe of returning values matching the lookup vector as well as the lookup vector values.
 #' @author Nick Bultman, \email{njbultman74@@gmail.com}, September 2020
-#' @seealso \code{\link{sum}}
+#' @seealso \code{\link{merge}}
 #' @keywords left join merge
 #' @export
 #' @examples
-#' count_if(iris$Species, "virginica")
-#' count_if(c(3, 1, 3), 3)
-#' 
+#' vlookup(c("Nick", "Jake", "Tyler", "Nate"), data.frame(name = c("Nick", "Jake", "Tyler", "Nate"), number = 1:4), "name", 2)
 #' @param lookup_vector Values that you would like to join on.
 #' @param lookup_df Data frame that contains values in lookup_vector and other values you would like to connect.
-#' @param join_col_name Name of the column that has values from lookup_vector.
+#' @param join_col_name Name of the column (as a string) that has values from lookup_vector.
 #' @param column_index Column in data frame that you would like returned once lookup value is found in lookup_df.
-#' 
+#'
 vlookup <- function(lookup_vector, lookup_df, join_col_name, column_index) {
-  
+
   column_name <- names(lookup_df)[column_index]
   df_1 <- data.frame(join_name = lookup_vector, stringsAsFactors = FALSE)
   names(df_1)[1] <- join_col_name
   df_2 <- merge(df_1, lookup_df, by = join_col_name)
-  
+
   df_2 <- df_2[, c(join_col_name, column_name)]
-  
+
   return(df_2)
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # xlsxFunctions
 Use functions from Excel in R.
+
+This package holds a suite of function names that you would commonly use in Microsoft Excel. 
+Oftentimes, it can be difficult to transition to a different technology because one has to 
+learn the names of new functions and their parameters. I attempt to make this transition 
+easier by providing commonly used functions in this package that are from Microsoft 
+Excel and able to be implemented in R.
