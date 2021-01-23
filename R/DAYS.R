19da23780e12bc941308c21fff52b4ffa4e5c382
njbultman74 <njbultman74@gmail.com>
2021-01-22 21:37:10 -0600

    Updating documentation month/year for DAYS function.

--- R/DAYS.R
+++ R/DAYS.R
@@ -3,7 +3,7 @@
 #' Given a start and end date, calculate the difference in days between the two.
 #'
 #' @return Returns the number of days between the start and end date.
-#' @author Nick Bultman, \email{njbultman74@@gmail.com}, December 2020
+#' @author Nick Bultman, \email{njbultman74@@gmail.com}, January 2021
 #' @seealso \code{\link{as.Date}}
 #' @keywords Date Difference Days
 #' @export
@@ -15,13 +15,13 @@
 #' @param start_date Start date in format "yyyy-mm-dd" as string.
 #'
 DAYS <- function(end_date, start_date) {
-  
+
   end_date <- as.Date(end_date)
-  
+
   start_date <- as.Date(start_date)
-  
+
   y <- end_date - start_date
-  
+
   return(as.numeric(y, units = "days"))
-  
+
 }
